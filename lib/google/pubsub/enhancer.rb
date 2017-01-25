7c0befcefaa4bb5fb2083208bca282889abf247c
bejczib <bejczi.balint@gmail.com>
2017-01-25 15:37:42 +0100

    initial commit

---
+++ lib/google/pubsub/enhancer.rb
@@ -0,0 +1,9 @@
+require "google/pubsub/enhancer/version"
+
+module Google
+  module Pubsub
+    module Enhancer
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/google/pubsub/enhancer/version.rb
@@ -0,0 +1,7 @@
+module Google
+  module Pubsub
+    module Enhancer
+      VERSION = "0.1.0"
+    end
+  end
+end
