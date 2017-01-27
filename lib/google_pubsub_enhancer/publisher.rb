c699ac762e6703e83a1aea9cb86bae6cbb9e5f2f
bbejczi <bejczi.balint@gmail.com>
2017-01-27 15:20:02 +0100

    fix: cache pubsub instance initialization

--- lib/google_pubsub_enhancer/publisher.rb
+++ lib/google_pubsub_enhancer/publisher.rb
@@ -5,10 +5,11 @@ class GooglePubsubEnhancer::Publisher
     @short_topic_name = opts[:short_topic_name] || raise
     @full_topic_name = GooglePubsubEnhancer.name_by('topics',@short_topic_name)
     @messages_key = opts[:messages] || raise
+    @google_cloud_pubsub ||= Google::Cloud::Pubsub.new
   end
 
   def call(env)
-    google_cloud_pubsub.publish(@full_topic_name) do |publisher|
+    @google_cloud_pubsub.publish(@full_topic_name) do |publisher|
       [*env[@messages_key]].each do |m|
         publisher.publish(m)
       end
@@ -16,7 +17,4 @@ class GooglePubsubEnhancer::Publisher
     @app.call(env)
   end
 
-  def google_cloud_pubsub
-    Google::Cloud::Pubsub.new
-  end
 end
