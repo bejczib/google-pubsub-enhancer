454f82c422e0d8287a79255840f868db393704e3
bejczib <bejczi.balint@gmail.com>
2017-03-16 11:10:58 +0100

    chore: extract google messages so it can be used to test subscription acknowledgement

--- lib/google_pubsub_enhancer/spec.rb
+++ lib/google_pubsub_enhancer/spec.rb
@@ -5,12 +5,13 @@ module GooglePubsubEnhancer::Spec
       let(:pubsub) {double "pubsub"}
       let(:publisher) { double "publisher"}
       let(:subscription) { double 'subscription'}
+      let(:google_messages) {messages.map { |m| Google::Cloud::Pubsub::Message.new(m.to_s)}}
       before do
         ENV['PUBSUB_KEYFILE_JSON'] = JSON.dump(project_id: 'cica')
         allow(Google::Cloud::Pubsub).to receive(:new).and_return(pubsub)
         allow(pubsub).to receive(:publish).and_yield(publisher)
         allow(pubsub).to receive(:subscription).and_return subscription
-        allow(subscription).to receive(:pull).and_return(messages.map { |m| Google::Cloud::Pubsub::Message.new(m.to_s)},nil)
+        allow(subscription).to receive(:pull).and_return(google_messages, nil)
         allow(publisher).to receive(:publish)
         allow(subscription).to receive(:acknowledge)
       end
